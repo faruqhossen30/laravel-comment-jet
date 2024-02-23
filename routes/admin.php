94e51c2ece3329dc162d89e5d67fd3c1aca6b5c6
faruqhossen30 <faruqhossen30@gmail.com>
2024-02-23 17:13:43 +0600

    dashboard layout complete

--- routes/admin.php
+++ routes/admin.php
@@ -1,9 +1,8 @@
 <?php
+
+use App\Http\Controllers\Admin\DashboardController;
 use Illuminate\Support\Facades\Route;
 
-
-Route::group(['prefix' => 'admin', 'middleware' => ['auth']], function () {
-    Route::get('/dashboard', function () {
-        return view('dashboard');
-    })->name('dashboard');
+Route::group(['prefix' => 'admin', 'middleware' => 'auth'], function () {
+    Route::get('dashboard', [DashboardController::class, 'index'])->name('dashboard');
 });
