cb9659b0219f1b58f3391c94fc71254aa714ff94
faruqhossen30 <faruqhossen30@gmail.com>
2024-02-23 15:32:44 +0600

    homepage ui added

--- routes/web.php
+++ routes/web.php
@@ -15,7 +15,10 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('homepage');
+});
+Route::get('/test', function () {
+    return view('test');
 });
 
 Route::get('/dashboard', function () {
